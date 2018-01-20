4f78c23b9600ed7efef5b726e8bcd5d9225abb95
Justin Bangerter <jnbangerter@gmail.com>
2018-01-19 19:21:50 -0500

    styling + migration of content from old site

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 4.2'
-gem 'middleman-autoprefixer', '~> 2.7'
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
-gem 'therubyracer'
+gem 'middleman', '~> 3.4.0'
+gem 'middleman3-sassc'
+gem 'middleman-autoprefixer'
+#gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+#gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+gem 'uglifier'
+gem 'sassc'
+gem 'redcarpet'
--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+activate :directory_indexes
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
@@ -5,6 +8,30 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+
+helpers do
+    def link_to_post title
+        return link_to(title, '/posts/'+urlify(title) + '.html')
+    end
+
+    def urlify string
+      string.tr(
+        " ", "_"
+      ).sub(
+        "&", "and"
+      ).sub(
+        "@", "at"
+      ).tr(
+        "^A-Za-z0-9_", ""
+      ).sub(
+        /_{2,}/, "_"
+      ).downcase
+    end
+end
+
+configure :development do
+  set :debug_assets, true
+end
 
 configure :build do
   activate :minify_css
